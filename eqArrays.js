bb4b7585dd9c20d7049b9956463d1a364a3a99f5
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-15 22:26:29 +0000

    implemented array checking function

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  console.log(arr1, arr2);
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([2, 2, 3], [1, 2, 3]), true);
