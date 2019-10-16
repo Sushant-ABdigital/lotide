c9a4cf0b349639fd05f6826fe4d4d919291d583f
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-16 00:30:23 +0000

    implemented flatten function

---
+++ flatten.js
@@ -0,0 +1,14 @@
+//Creating function to flatten an array
+const flatten = arr => {
+  let output = [];
+  arr.forEach(el => {
+    if (!Array.isArray(el)) {
+      output.push(el);
+    } else {
+      el.forEach(e => output.push(e));
+    }
+  });
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
