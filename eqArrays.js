752291b07fa6a454a3a682b425c54d80c793979b
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-21 15:58:25 +0000

    Adding multiline comment
    1. Refactored eqArrays.js file by removing test and exporting module
    2. Added test file as a part of refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
@@ -16,5 +16,7 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([2, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
+// //Test
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([2, 2, 3], [1, 2, 3]), true);
