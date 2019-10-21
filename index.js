b0a588107d641f80e54a14c2e1df1524fd3d8b01
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-21 20:37:04 +0000

    Refactored the index file with newer object syntax

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const flatten = require('./flatten');
-const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const recursiveEqArrays = require('./recursiveEqArrays');
-const recursiveEqObject = require('./recursiveEqObject');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const flatten = require("./flatten");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const recursiveEqArrays = require("./recursiveEqArrays");
+const recursiveEqObject = require("./recursiveEqObject");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual,
+  tail,
+  middle,
   countLetters,
   countOnly,
   eqArrays,
@@ -37,5 +34,5 @@ module.exports = {
   recursiveEqArrays,
   recursiveEqObject,
   takeUntil,
-  without,
+  without
 };
