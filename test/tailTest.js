f822de0ed1a790802691a41d97f31a230620959f
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-21 19:35:28 +0000

    Added M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,17 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
+
+// //Test
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs");
+
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-//Test
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("should return the last element", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
