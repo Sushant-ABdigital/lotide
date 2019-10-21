27976f31d5c4dac445c2f5bd5ac1a9de80d22cb9
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-21 16:57:02 +0000

    Refactored middle function with tests

--- middle.js
+++ middle.js
@@ -1,21 +1,22 @@
 //Testing
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
+// const eqArrays = (arr1, arr2) => {
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) return false;
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log("Both the arrays are same");
-  } else {
-    console.log("Both the arrays are not same!");
-  }
-};
+// const assertArraysEqual = (arr1, arr2) => {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log("Both the arrays are same");
+//   } else {
+//     console.log("Both the arrays are not same!");
+//   }
+// };
+// const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //Middle function to get the middle value from an array.
-
 const middle = arr => {
   const midValue = Math.ceil(arr.length / 2);
   const output = [];
@@ -34,7 +35,8 @@ const middle = arr => {
   }
 };
 
-console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
+// console.log(middle([1, 2, 3, 4, 5, 6]));
 
-//Testing by adding own tests
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// //Testing by adding own tests
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
