ec310d742914672297b978e2fb9071513f5324ae
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-21 19:44:20 +0000

    Adding M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,14 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-//Testing by adding own tests
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// //Testing by adding own tests
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+//Adding M & C
+const assert = require("chai").assert;
+const middle = require("../middle");
+
+describe("#middleTest", () => {
+  it("should give the middle element from an array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
